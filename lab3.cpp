9fbac4e0263ded00f6031db9d91d0b731608c37e
kzawislinski <k.zawislinski@gmail.com>
2020-11-12 19:39:09 +0000

    zad. 10

--- lab3.cpp
+++ lab3.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
 
+struct Figura
+{
+public:
+    Figura(double p) { pole = p; }
+    double getPole() { return pole; }
+    void   id() const { std::cout << "Figura o polu " << pole << '\n'; };
+
+private:
+    double pole;
+};
+
+struct Kolo : public Figura
+{
+    Kolo(double r) : Figura{3.14 * r * r} {};
+    void id() { std::cout << "Kolo o polu " << getPole() << '\n'; };
+};
+
+struct Kwadrat : public Figura
+{
+    Kwadrat(double a) : Figura{a * a} {};
+
+    void id() { std::cout << "Kwadrat o polu " << getPole() << '\n'; };
+};
+
+void id(const Figura& f)
+{
+    f.id();
+}
+
 int main()
 {
-    puts("Hello, World!");
+    Kwadrat m{4};
+    m.id();
+    id(static_cast< Figura >(m));
 }
