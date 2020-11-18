f01c7e3b68c714beaa4fa4d82de7a3e802528078
kzawislinski <k.zawislinski@gmail.com>
2020-11-18 16:16:53 +0000

    zal

--- lab3.cpp
+++ lab3.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 
-struct Figura
+struct BytGeometryczny
+{
+    virtual void id() = 0;
+};
+
+struct Figura : public BytGeometryczny
 {
 public:
     Figura(double p) { pole = p; }
     double getPole() { return pole; }
-    void   id() const { std::cout << "Figura o polu " << pole << '\n'; };
+    // virtual void id() { std::cout << "Figura o polu " << pole << '\n'; };
+
+    virtual ~Figura() { std::cout << "Figura zostala zniszczona\n"; };
 
 private:
     double pole;
@@ -14,24 +21,28 @@ private:
 struct Kolo : public Figura
 {
     Kolo(double r) : Figura{3.14 * r * r} {};
-    void id() { std::cout << "Kolo o polu " << getPole() << '\n'; };
+    void id() override { std::cout << "Kolo o polu " << getPole() << '\n'; };
+    virtual ~Kolo() { std::cout << "Kolo zostalo zniszczone\n"; };
 };
 
 struct Kwadrat : public Figura
 {
     Kwadrat(double a) : Figura{a * a} {};
 
-    void id() { std::cout << "Kwadrat o polu " << getPole() << '\n'; };
+    void id() override { std::cout << "Kwadrat o polu " << getPole() << '\n'; };
+
+    virtual ~Kwadrat() { std::cout << "Kwadrat zostal zniszczony \n"; };
 };
 
-void id(const Figura& f)
+void id(Figura& f)
 {
     f.id();
 }
 
 int main()
 {
-    Kwadrat m{4};
+    Kwadrat m(4);
     m.id();
-    id(static_cast< Figura >(m));
+    id(m);
+    // std::cout << dynamic_cast< Kolo* >(m);
 }
--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -4,6 +4,40 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
+  if (towar > 0)
+  {
+    int sum =0;
+    
+    int i =0;
+    
+    int zagl =0;
+    
+    int temp;
+
+    Stocznia stocznia{};
+    
+    Statek* s1;
+   
+    while ( sum < towar && i <100000)
+    {
+      s1 = stocznia();
+      temp = s1->transportuj(); 
+      sum = sum + temp;
+      i = i +1;
+      s1=dynamic_cast<Zaglowiec*>(s1);
+      if(s1)
+    {
+      zagl = zagl +1;
+    }
+    delete s1;
+    }
+
+   return zagl;
+  }
+
+  else
+  {
     return 0;
+  }
+
 }
